f4ac1fc1b4c43814c6f823b4fb0ddc0fc72fcbeb
gamenarkyt <gagaritin11@gmail.com>
2023-11-27 17:35:19 +0300

    move from css to scss

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./styles/styles.css";
-import { RouterProvider } from "react-router-dom";
-import { router } from "@/pages";
+import React from "react"
+import ReactDOM from "react-dom/client"
+import "./styles/styles.scss"
+import { RouterProvider } from "react-router-dom"
+import { router } from "@/pages"
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>
-);
+  </React.StrictMode>,
+)
