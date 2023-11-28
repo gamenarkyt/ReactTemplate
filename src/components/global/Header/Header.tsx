1117704570af22b859f318ca9daa7350489e9afb
gamenarkyt <gagaritin11@gmail.com>
2023-11-28 15:31:59 +0300

    Revert to normal header

--- src/components/global/Header/Header.tsx
+++ src/components/global/Header/Header.tsx
@@ -4,16 +4,6 @@ export const Header = () => {
   return (
     <header className={styles.header}>
       <span>SiteName</span>
-      <span>menu</span>
-      <span>menu</span>
-      <span>menu</span>
-      <span>menu</span>
-      <span>menu</span>
-      <span>menu</span>
-      <span>menu</span>
-      <span>menu</span>
-      <span>menu</span>
-      <span>menu</span>
     </header>
   )
 }
