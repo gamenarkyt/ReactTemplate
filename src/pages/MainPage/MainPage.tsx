2646279b129ec3248e19e5414a817dfe70136987
gamenarkyt <gagaritin11@gmail.com>
2023-11-28 16:37:32 +0300

    some test changes for pages

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,3 +1,19 @@
+import styles from "./MainPage.module.scss"
 export const MainPage = () => {
-  return <div>MainPage</div>
+  return (
+    <div>
+      <div className={styles.ll}>12123123</div>
+      <div className={styles.ll}>12123123</div>
+      <div className={styles.ll}>12123123</div>
+      <div className={styles.ll}>12123123</div>
+      <div className={styles.ll}>12123123</div>
+      <div className={styles.ll}>12123123</div>
+      <div className={styles.ll}>12123123</div>
+      <div className={styles.ll}>12123123</div>
+      <div className={styles.ll}>12123123</div>
+      <div className={styles.ll}>12123123</div>
+      <div className={styles.ll}>12123123</div>
+      <div className={styles.ll}>12123123</div>
+    </div>
+  )
 }
--- src/pages/SecondPage/SecondPage.tsx
+++ src/pages/SecondPage/SecondPage.tsx
@@ -1,3 +1,7 @@
 export const SecondPage = () => {
-  return <div>SecondPage</div>
+  return (
+    <div>
+      <span>second page</span>
+    </div>
+  )
 }
